9a3a1d77e4f39150e472e34fd58e05abea2762b6
Fazreen Aina <2021155999@student.uitm.edu.my>
2022-01-24 16:51:42 +0800

    add postgrest

---
+++ src/main/java/com/example/librarysystem/connection/Postgres.java
@@ -0,0 +1,59 @@
+package com.example.librarysystem.connection;
+
+import java.net.URI;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+
+public class Postgres {
+    private static Connection conn = null;
+
+    private static void initDb() {
+
+        try
+        {
+            URI dbUri = null;
+            if(System.getenv("DATABASE_URL") != null)
+            {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            else{
+                //jdbc:postgresql://<host>:<port>/<dbname>?user=<username>&password=<password>
+                dbUri = new URI("jdbc:postgresql://postgres:system@localhost:5432/mvcdemo");
+            }
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+            conn =  DriverManager.getConnection(dbUrl, username, password);
+        }
+        catch (Exception err) {
+            err.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        if (conn == null)
+        {
+            //initialize db
+            initDb();
+        }
+        return conn;
+    }
+
+    public static void closeConnection()
+    {
+        try{
+            if(conn.isClosed() || conn != null)
+            {
+                conn.close();
+            }
+        }
+        catch(Exception err)
+        {
+
+        }
+    }
+}
